bbe34597f896161e0d68e04fe92995b0bf2542d4
Marcos Alarcon <proveedormarcos.alarcon@sura.pe>
2023-04-21 17:44:54 -0500

    add load balancer in ms-customer

--- customer/src/main/java/org/malarcondev/customer/service/CustomerService.java
+++ customer/src/main/java/org/malarcondev/customer/service/CustomerService.java
@@ -27,7 +27,7 @@ public class CustomerService {
         // TODO: check if email not taken
         customerRepository.saveAndFlush(customer);
         FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                "http://FRAUD/api/v1/fraud-check/{customerId}",
                 FraudCheckResponse.class,
                 customer.getId()
         );
