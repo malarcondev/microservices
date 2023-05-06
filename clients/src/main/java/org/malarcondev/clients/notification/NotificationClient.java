d550ebdff0eec9b0ac12f94432598fde28b189c9
Marcos Alarcon <proveedormarcos.alarcon@sura.pe>
2023-05-06 16:02:16 -0500

    add k8s for ms

--- clients/src/main/java/org/malarcondev/clients/notification/NotificationClient.java
+++ clients/src/main/java/org/malarcondev/clients/notification/NotificationClient.java
@@ -3,7 +3,10 @@ package org.malarcondev.clients.notification;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient("notification")
+@FeignClient(
+        name = "notification",
+        url = "clients.notification.url="
+)
 public interface NotificationClient {
     @PostMapping("api/v1/notification")
     void sendNotification(NotificationRequest notificationRequest);
